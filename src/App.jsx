09f5a549a142fc99488134b554ad9ee0718ed180
Nicolas Arcamone <109641395+nicolasArcamone97@users.noreply.github.com>
2026-01-22 11:34:37 -0300

    docs modificacion

--- src/App.jsx
+++ src/App.jsx
@@ -128,9 +128,9 @@ function App() {
               <h1 className="text-xl md:text-2xl font-bold text-white">
                 UNIVERSIDAD NACIONAL DE HURLINGHAM
               </h1>
-              <p className="text-white/90 text-sm md:text-base mt-1">
+              {/* <p className="text-white/90 text-sm md:text-base mt-1">
                 "Conocimiento para la inclusión social"
-              </p>
+              </p> */}
             </div>
           </div>
           <div className="flex items-center gap-2 shadow-md px-4 py-2 rounded-lg bg-white/20 backdrop-blur-sm text-white font-medium text-sm md:text-base">
